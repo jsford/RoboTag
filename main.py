5ae8c7ca098ac1c60eb4641b2ba38ed2cbd0529e
Jordan Ford <jsford94@gmail.com>
2016-10-16 01:27:02 -0400

    Added calculation of path cost to reconstruct_path. astar_solve now returns the path and its cost.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
+from astar import *
+from djikstra import *
 from world import World
+
+DISPLAY_SEARCH = True
+
 
 if __name__ == "__main__":
 
@@ -8,11 +13,19 @@ if __name__ == "__main__":
         exit(1)
 
     # Construct the world
-    world = World(sys.argv[1], verbose=True)
+    world = World(sys.argv[1], verbose=DISPLAY_SEARCH)
 
-    #(path, cost) = world.intercept()
+    for i in range(0, len(world.path), 250):
+        astar_path, cost = astar_solve(world, tuple(world.start), tuple(world.path[i]), weight=100, verbose=DISPLAY_SEARCH)
     
-        
+        print "Path Cost: ", cost
+        for loc in astar_path:
+            print loc
+        for l in astar_path:
+            world.img[l] = [255, 0, 255, 255]
+
+    while(1):
+        world.update_display()
     
     
 
--- world.py
+++ world.py
@@ -1,9 +1,6 @@
 import numpy as np
-import numpy.ma as ma
 import matplotlib.pyplot as plt
 import cv2 as cv
-import time
-import heapq as hq
 
 class World:
     def __init__(self, fname, verbose=False):
@@ -38,23 +35,8 @@ class World:
             line = [int(w) for w in line]
             self.costmap[x,:] = np.array(line)
 
-        #for i in range(0, self.N):
-        #    for j in range(0, self.N):
-        #        if self.costmap[i,j] < 200:
-        #            self.costmap[i,j] = 1
-            
-
+        # Initialize the display.
         self._init_display()
-
-        # SET_END
-        for i in range(0, len(self.path), 250):
-            astar_path = self.astar_solve(tuple(self.start), tuple(self.path[i]), 100)
-            print astar_path
-            for l in astar_path:
-                self.img[l] = [255, 0, 255, 255]
-
-        while(1):
-            self._update_display()
 
     def _init_display(self):
         max = np.max(self.costmap)
@@ -70,7 +52,7 @@ class World:
 
         self.img[self.start[0]][self.start[1]] = [0,0,255,255]
     
-    def _update_display(self):
+    def update_display(self):
         if(self.verbose):
             img = self.img
             img = cv.resize(self.img, (1000,1000), interpolation=cv.INTER_NEAREST)
@@ -82,123 +64,6 @@ class World:
                 cv.destroyAllWindows()
                 exit(0)
 
-    def djikstra_solve(self):
-        Q = set(range(0, self.N**2))
-        dist = np.full(self.N**2, np.inf)
-        mask = np.full((self.N**2), 0)
-        dist = ma.masked_array(dist, mask)
-        prev = np.full(self.N**2, -1)
-        prev[0] = 0
-        
-        dist[self.start[0]*self.N + self.start[1]] = 0 
-       
-        count = 0 
-        while( len(Q) > 0 ):
-            u = np.argmin(dist)
+    
 
-            self.img[int(u/self.N), u%self.N] = [0,255,0,255]
-            
-            if( count == 1000 ): 
-                self._update_display()
-                count = 0
-            count += 1
-            
-            for v in [u-1, u+1, u-self.N, u+self.N]:
-                if( not (v in Q) or v < 0 or v >= self.N**2
-                    or (v==u-1 and u%self.N==0) 
-                    or (v==u+1 and u%self.N==self.N-1) ):
-                    continue
-                alt = dist[u] + self.costmap[v] 
-                if alt < dist[v]:
-                    dist[v] = alt
-                    prev[v] = u
 
-            Q.remove(u)
-            dist.mask[u] = 1
-
-        print dist.reshape((self.N, self.N))
-        print prev.reshape((self.N, self.N))
-        print 
-
-    def manhattan_dist(self, start, end):
-        return abs(start[0] - end[0]) + abs(start[1] - end[1])
-
-    def euclidean_dist(self, start, end):
-        return np.sqrt((start[0]-end[0])**2 + (start[1]-end[1])**2)
-
-    def reconstruct_path(self, prev, current):
-        current = current
-        total_path = [current]
-        while True:
-            current = prev[current]
-            if current == None: break
-            total_path.append(current)
-        total_path.reverse()
-        return total_path
-
-    def get_successors(self, node):
-        neighbors = []
-        for neighbor in [(node[0]+1, node[1]  ), (node[0]-1, node[1]  ), 
-                         (node[0]  , node[1]-1), (node[0]  , node[1]+1)]:
-
-            if(neighbor[0] >= self.N or neighbor[1] >= self.N or 
-                neighbor[0] < 0 or neighbor[1] < 0):
-                continue
-            neighbors.append(neighbor)
-        return neighbors
-
-    def astar_solve(self, start, end, weight=1):
-
-        closed_set = set([])
-        open_set = []
-        hq.heappush(open_set, (0, start)) # Don't need to calculate heuristic bc any value will be the min.
-
-        prev   = {} 
-        prev[start] = None
-        gscore = {} 
-        gscore[start] = 0
-        
-        count = 0
-        while open_set:
-            # Pick the frontier node with smallest fscore.
-            curr = hq.heappop(open_set)[-1]
-
-            # If curr has been explored previously, skip it.
-            if curr in closed_set:
-                continue
-
-            # Bullshit for debug
-            if count % 1000 == 0 or curr == end:
-                for o in open_set:
-                    self.img[o[-1]] = [0, 0, 255, 255]
-                for c in closed_set:
-                    self.img[c] = [0, 255, 0, 255]
-                self._update_display()
-            count += 1
-
-            # If we found the goal, quit.
-            if curr == end:
-                print "DONE! " + str(count) + " states"
-                return self.reconstruct_path(prev, curr)
-
-            # Add this node to the closed set.
-            closed_set.add(curr)
-            # Expand this node.
-            for neighbor in self.get_successors(curr):
-                # Skip if we've already expanded this neighbor
-                if neighbor in closed_set: continue 
-                # Find the cost of the cheapest path to neighbor via curr
-                new_gscore = gscore[curr] + self.costmap[neighbor[0], neighbor[1]]
-                # If neighbor is new or we found a cheaper path
-                if neighbor not in open_set or new_gscore < gscore[neighbor]:
-                    # fscore is g+h
-                    fscore = new_gscore + weight*self.euclidean_dist(neighbor, end)
-                    # Add neighbor to the frontier
-                    hq.heappush(open_set, (fscore, neighbor))
-                    # Keep track of its predecessor
-                    prev[neighbor] = curr
-                    # Update its gscore
-                    gscore[neighbor] = new_gscore
-
-        print 'FAILED!'
-        return
