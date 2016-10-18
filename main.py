ae61d005ea05891015979fd50b6ee8041f9a6ebe
Jordan Ford <jsford94@gmail.com>
2016-10-18 04:05:00 -0400

    Working A* and Djikstra. MUCH faster due to use of correct data structures.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
-from astar import *
-from djikstra import *
-from world import World
+import numpy as np
 import time
+from world import World
 
 DISPLAY_SEARCH = False
 
+def insert_dwell(path, dwell_loc, dwell_amt):
+    for loc in range(0, len(path)):
+        location = path[loc]
+        if(location == dwell_loc):
+            for d in range(0, dwell_amt):
+                min_intercept_path.insert(loc, dwell_loc)
+    return path
+    
 
 if __name__ == "__main__":
 
+    start = time.time()
     if len(sys.argv) != 2:
         print 'usage: python chase.py prob.txt'
         exit(1)
@@ -16,25 +24,56 @@ if __name__ == "__main__":
     # Construct the world
     world = World(sys.argv[1], verbose=DISPLAY_SEARCH)
 
-    for i in range(0, len(world.path), 250):
-        start = time.time()
-        astar_path, cost = astar_solve(world, tuple(world.path[i]), tuple(world.start), weight=1, verbose=DISPLAY_SEARCH)
-        end = time.time()
-        break
-    
-    print "ELAPSED TIME: ", (end-start)
-    print "Unweighted Path Cost: ", cost
-    for l in astar_path:
-        world.img[l] = [255, 0, 255, 255]
+    unique_locations = set([])
+    for i in range(0, len(world.path)):
+        if world.manhattan_dist(world.path[i], world.start) > i \
+           or world.path[i] in unique_locations: 
+            continue
+        unique_locations.add((i, world.path[i]))
 
-    for loc in astar_path:
-        print loc
-       
-    if(DISPLAY_SEARCH):
-        while(1):
-            world.update_display()
-    
-    
+    dist, prev = world.djikstra_solve()
 
+    min_intercept_path = None
+    min_intercept_cost = np.inf
+    min_intercept_dwell_cost = None
+    min_intercept_dwell_loc = None
+    min_intercept_time = np.inf
     
-      
+    unique_locations = list(unique_locations)
+
+    for R2D2_idx in range(0, len(unique_locations)):
+        (R2D2_time, R2D2_loc) = unique_locations[R2D2_idx]
+
+        path, cost, dwell_cost, dwell_loc = world.reconstruct_path(prev, R2D2_loc)
+        if( R2D2_time < len(path) ): continue
+
+        cost += (R2D2_time-len(path))*dwell_cost
+
+        if(cost < min_intercept_cost or (cost == min_intercept_cost and R2D2_time < min_intercept_time) ):
+            min_intercept_cost = cost
+            min_intercept_time = R2D2_time
+            min_intercept_dwell_cost = dwell_cost
+            min_intercept_dwell_loc = dwell_loc
+            min_intercept_path = path
+
+
+    if(min_intercept_time != len(min_intercept_path)):
+        #print "DWELL at " + str(min_intercept_dwell_loc) + " for " + str(min_intercept_time-len(min_intercept_path)) + " timesteps at a cost of " + str(min_intercept_dwell_cost) + " per timestep."
+        world.insert_dwell(path, min_intercept_dwell_loc, min_intercept_time-len(min_intercept_path))
+
+
+    print min_intercept_cost
+    for l in min_intercept_path:
+        print l
+    print "Time: ", time.time()-start
+
+
+
+
+
+
+
+
+
+
+
--- world.py
+++ world.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
+import heapq as hq
 
 class World:
     def __init__(self, fname, verbose=False):
@@ -15,7 +16,7 @@ class World:
         # Get R
         f.readline().strip()
         self.start = f.readline().strip().split(',')
-        self.start = [int(c) for c in self.start]
+        self.start = tuple([int(c) for c in self.start])
 
         # Get T
         f.readline().strip()
@@ -24,9 +25,8 @@ class World:
             next_pos = f.readline().strip()
             if next_pos == 'B': break
             next_pos = next_pos.split(',')
-            next_pos = [int(c) for c in next_pos]
+            next_pos = tuple([int(c) for c in next_pos])
             self.path.append(next_pos)
-
 
         # Get B
         self.costmap = np.empty(shape=(self.N, self.N), dtype = np.int64)
@@ -68,6 +68,121 @@ class World:
         (x, y) = point
         return 0 <= x < self.N and 0 <= y < self.N
 
-    
+    def manhattan_dist(self, start, end):
+        return abs(start[0] - end[0]) + abs(start[1] - end[1])
 
+    def reconstruct_path(self, prev, current):
+        cost = self.costmap[current]
+        total_path = [current]
+        dwell_cost = np.inf
+        dwell_loc  = None
 
+        while True:
+            if self.costmap[current] < dwell_cost:
+                dwell_cost = self.costmap[current] 
+                dwell_loc  = current
+            current = prev[current]
+            if current == None: break
+            cost += self.costmap[current]
+            total_path.append(current)
+        total_path.reverse()
+        return total_path, cost, dwell_cost, dwell_loc
+
+    def astar_solve(self, start, end, weight=1):
+
+        closed_set = set([])
+        open_set = []
+        hq.heappush(open_set, (0, start)) # Don't need to calculate heuristic bc any value will be the min.
+
+        prev   = {} 
+        prev[start] = None
+        gscore = {} 
+        gscore[start] = 0
+
+        count = 0
+
+        h = np.empty((self.N, self.N), dtype=np.int)
+        for i in range(0, self.N):
+            for j in range(0, self.N):
+                h[i][j] = manhattan_dist((i,j), end)
+
+        while open_set:
+            # Pick the frontier node with smallest fscore.
+            curr = hq.heappop(open_set)[-1]
+
+            # If curr has been explored previously, skip it.
+            if curr in closed_set:
+                continue
+
+            # Bullshit for debug
+            if self.verbose and (count % 1000 == 0 or curr == end):
+                for o in open_set:
+                    self.img[o[-1]] = [0, 0, 255, 255]
+                for c in closed_set:
+                    self.img[c] = [0, 255, 0, 255]
+                self.update_display()
+            count += 1
+
+            # If we found the goal, quit.
+            if curr == end:
+                return reconstruct_path(prev, curr, self.costmap)
+
+            # Add this node to the closed set.
+            closed_set.add(curr)
+            # Expand this node.
+            for neighbor in filter(self.in_bounds, [(curr[0]+1, curr[1]  ), (curr[0]-1, curr[1]  ), (curr[0]  , curr[1]-1), (curr[0]  , curr[1]+1)]):
+
+                # Skip if we've already expanded this neighbor
+                if neighbor in closed_set: continue 
+                # Find the cost of the cheapest path to neighbor via curr
+                new_gscore = gscore[curr] + self.costmap[neighbor[0], neighbor[1]]
+                # If neighbor is new or we found a cheaper path
+                if neighbor not in gscore or new_gscore < gscore[neighbor]:
+                    # fscore is g+h
+                    fscore = new_gscore + weight*h[neighbor]
+                    # Add neighbor to the frontier
+                    hq.heappush(open_set, (fscore, neighbor))
+                    # Keep track of its predecessor
+                    prev[neighbor] = curr
+                    # Update its gscore
+                    gscore[neighbor] = new_gscore
+
+        print 'FAILED!'
+        return None
+
+    def djikstra_solve(self):
+        Q = []
+        dist = np.full((self.N, self.N), np.inf)
+        dist[self.start] = 0
+
+        for i in range(0, self.N):
+            for j in range(0, self.N):
+                if (i,j) == self.start:
+                    hq.heappush(Q, (0, self.start))
+                else:
+                    hq.heappush(Q, (np.inf, (i,j)))
+
+        prev = {}
+        prev[self.start] = None
+       
+        count = 0 
+        while len(Q) > 0:
+            d, u = hq.heappop(Q)
+
+            for v in filter(self.in_bounds, [(u[0]+1, u[1]), (u[0]-1, u[1]), 
+                                             (u[0], u[1]+1), (u[0], u[1]-1)]):
+                alt = d + self.costmap[v] 
+                if alt < dist[v]:
+                    hq.heappush(Q, (alt, v))
+                    dist[v] = alt
+                    prev[v] = u
+
+        return dist, prev
+
+    def insert_dwell(self, path, dwell_loc, dwell_amt):
+        for loc in range(0, len(path)):
+            location = path[loc]
+            if(location == dwell_loc):
+                for d in range(0, dwell_amt):
+                    min_intercept_path.insert(loc, dwell_loc)
+        return path
